d0d8cfbf3ab521f31459fe8f0e51357c68a2a565
Emiel Kalksma <emiel.kalksma@gmail.com>
2020-04-24 15:55:59 +0200

    Basic game setup

--- src/game.js
+++ src/game.js
@@ -1,6 +1,13 @@
-class Game {
+export default class Game {
   constructor() {
+    this.canvas = document.getElementById('canvas');
+    this.ctx = this.canvas.getContext('2d');
 
+    this.tileSize = 8;
+    this.gridSize = {x: 20, y: 20};
+
+    this.canvas.width = this.gridSize.x * this.tileSize;
+    this.canvas.height = this.gridSize.y * this.tileSize;
   }
 
   update() {
@@ -8,17 +15,19 @@ class Game {
   }
 
   draw() {
-
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.fillStyle = "#FF0000";
+    this.ctx.fillRect(10 * this.tileSize, 1 * this.tileSize, this.tileSize, this.tileSize);
   }
 
   run() {
-    this.update();
-    this.draw();
-
     window.requestAnimationFrame(this.loop.bind(this));
   }
 
   loop() {
+    this.update();
+    this.draw();
+
     window.requestAnimationFrame(this.loop.bind(this));
   }
 }
