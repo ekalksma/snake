b8d29cbef3aaf877c9e7238148b37fd4b2b3b09f
Emiel Kalksma <emiel.kalksma@gmail.com>
2020-04-24 16:34:54 +0200

    Add input handeling

--- src/game.js
+++ src/game.js
@@ -8,16 +8,49 @@ export default class Game {
 
     this.canvas.width = this.gridSize.x * this.tileSize;
     this.canvas.height = this.gridSize.y * this.tileSize;
+
+    this.position = {
+      x: Math.random() * this.gridSize.x,
+      y: Math.random() * this.gridSize.y
+    };
+    this.dv = {
+      x: 0,
+      y: 0
+    };
+
+    window.addEventListener('keypress', this.handleKeyPress.bind(this));
+
+    setInterval(this.update.bind(this), 500);
+  }
+
+  handleKeyPress(event) {
+    if (event.key === 'w' && this.dv.y === 0) {
+      this.dv.x = 0;
+      this.dv.y = -1;
+    }
+    if (event.key === 's' && this.dv.y === 0) {
+      this.dv.x = 0;
+      this.dv.y = 1;
+    }
+    if (event.key === 'a' && this.dv.x === 0) {
+      this.dv.x = -1;
+      this.dv.y = 0;
+    }
+    if (event.key === 'd' && this.dv.x === 0) {
+      this.dv.x = 1;
+      this.dv.y = 0;
+    }
   }
 
   update() {
-
+    this.position.x += this.dv.x;
+    this.position.y += this.dv.y;
   }
 
   draw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     this.ctx.fillStyle = "#FF0000";
-    this.ctx.fillRect(10 * this.tileSize, 1 * this.tileSize, this.tileSize, this.tileSize);
+    this.ctx.fillRect(this.position.x * this.tileSize, this.position.y * this.tileSize, this.tileSize, this.tileSize);
   }
 
   run() {
@@ -25,7 +58,6 @@ export default class Game {
   }
 
   loop() {
-    this.update();
     this.draw();
 
     window.requestAnimationFrame(this.loop.bind(this));
